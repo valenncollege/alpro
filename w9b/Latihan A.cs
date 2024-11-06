4cff6ae98511475c3a6142d4f7ade09baee8c3f9
valenn <s160424066@student.ubaya.ac.id>
2024-11-06 21:02:10 +0700

    Add files via upload

---
+++ w9b/Latihan A.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_W9B_Jevon_Valentino_160424066
+{
+    public partial class Latihan_A : Form
+    {
+        public Latihan_A()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrNilai = new int[30];
+        int jumlahNilaiInput = 0;
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            int nilai = (int)nudNilai.Value;
+            arrNilai[jumlahNilaiInput] = nilai;
+
+            jumlahNilaiInput++;
+
+            lstHasil.Items.Add("Nilai mhs "+ jumlahNilaiInput + " : "+ nilai);
+
+            MessageBox.Show("Nilai telah tersimpan");
+        }
+
+        private void Latihan_A_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTampil_Click(object sender, EventArgs e)
+        {
+            lstHasil.Items.Clear();
+
+            lstHasil.Items.Add("Daftar Nilai Mahasiswa");
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                lstHasil.Items.Add("Nilai mhs " + (i+1)  + ": " + arrNilai[i]);
+            }
+
+        }
+
+        private void btnRatarata_Click(object sender, EventArgs e)
+        {
+            lstHasil.Items.Clear();
+            double total = 0;
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                total = total + arrNilai[i];
+            }
+
+            double rata = total / jumlahNilaiInput;
+            for (int j= 0; j < jumlahNilaiInput; j++)
+            {
+                lstHasil.Items.Add("Nilai mhs " + (j + 1) + ": " + arrNilai[j]);
+            }
+            lstHasil.Items.Add("Rata2 Nilai Mahasiswa = " + rata);
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            lstHasil.Items.Clear();
+            int min = 100;
+            int min2=0;
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                if (min > arrNilai[i])
+                {
+                    min = arrNilai[i];
+                }
+            }
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                if (min == arrNilai[i])
+                {
+                    min2 = i + 1;
+                }
+            }
+
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                lstHasil.Items.Add("Nilai mhs " + (i + 1) + ": " + arrNilai[i]);
+            }
+            lstHasil.Items.Add("Nilai min mahasiswa : " + min);
+            lstHasil.Items.Add("Beliau adalah mhs ke-" + min2);
+
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            lstHasil.Items.Clear();
+            int max = 0;
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                if (max < arrNilai[i])
+                {
+                    max = arrNilai[i];
+                }
+            }
+            for (int i = 0; i < jumlahNilaiInput; i++)
+            {
+                lstHasil.Items.Add("Nilai mhs " + (i + 1) + ": " + arrNilai[i]);
+            }
+            lstHasil.Items.Add("Nilai min mahasiswa : " + max);
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLaporan_Click(object sender, EventArgs e)
+        {
+            int remidi = 0;
+            int beliau=0;
+            for (int i = 0; i <jumlahNilaiInput; i++)
+            {
+                if(arrNilai[i] < 55)
+                {
+                    beliau = i + 1;
+                    lstHasil.Items.Add("Mhs ke " + beliau +" tidak lulus karena mendapat nilai "+ arrNilai[i]);
+                    remidi++;
+                }
+            }
+            lstHasil.Items.Add("Jumlah mhs yg di bawah 55 : " + remidi);
+
+            
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int min, indekmin;
+
+            for (int j = 0; j < jumlahNilaiInput-1; j++)
+            {
+                min = arrNilai[j];
+                indekmin = j;
+
+                for (int i = j+1; i < jumlahNilaiInput; i++)
+                {
+                    if (min > arrNilai[i])
+                    {
+                        min = arrNilai[i];
+                        indekmin = 1;
+                    }
+                }
+                //tukar
+                int temp = arrNilai[j];
+                arrNilai[j] = arrNilai[indekmin];
+                arrNilai[indekmin] = temp;
+            }
+        }
+    }
+}
---
+++ w9b/no1-4.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_W9B_Jevon_Valentino_160424066
+{
+    public partial class tugas_w9b : Form
+    {
+        public tugas_w9b()
+        {
+            InitializeComponent();
+        }
+        int[] arrTanggal = new int[30];
+        int[] arrBulan = new int[12];
+        int[] arrJumlah = new int[100];
+        int jumlahInput = 0;
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            int tanggal = int.Parse(txtTanggal.Text);
+            int bulan = int.Parse(txtBulan.Text);
+            int jumlahPenjualan = int.Parse(txtJumlah.Text);
+
+            arrTanggal[jumlahInput] = tanggal;
+            arrBulan[jumlahInput] = bulan;
+            arrJumlah[jumlahInput] = jumlahPenjualan;
+
+            jumlahInput++;
+
+            txtTanggal.Clear();
+            txtBulan.Clear();
+            txtJumlah.Clear();
+            txtTanggal.Select();
+        }
+
+        private void btnTampil_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i < jumlahInput; i++)
+            {
+                lstOut.Items.Add("Tanggal : " + arrTanggal[i]);
+                lstOut.Items.Add("Bulan : " + arrBulan[i]);
+                lstOut.Items.Add("Jumlah penjualan : " + arrJumlah[i]);
+                lstOut.Items.Add("");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            if (txtBulan.Text == "")
+            {
+                for (int i = 0; i < jumlahInput; i++)
+                {
+                    total = total + arrJumlah[i];
+                }
+                lstOut.Items.Add("Total penjualan = " + total);
+
+            }
+            else
+            {
+                int bulan = int.Parse(txtBulan.Text);
+                for (int i = 0; i < jumlahInput; i++)
+                {
+                    if (arrBulan[i] == bulan)
+                    {
+                        total = total + arrJumlah[i];
+                    }
+                }
+                lstOut.Items.Add("Total penjualan = " + total);
+            }
+
+        }
+
+        private void btnRata_Click(object sender, EventArgs e)
+        {
+            double total = 0, rata=0;
+            if (txtBulan.Text == "")
+            {
+                for (int i = 0; i < jumlahInput; i++)
+                {
+                    total = total + arrJumlah[i];
+                }
+                rata = total / jumlahInput;
+            }
+            else
+            {
+                int count = 0;
+                int bulan = int.Parse(txtBulan.Text);
+                for (int i = 0; i < jumlahInput; i++)
+                {
+                    if (arrBulan[i] == bulan)
+                    {
+                        total = total + arrJumlah[i];
+                        count++;
+                    }
+                }
+                rata = total / count;
+            }
+            double hasilRata = Math.Round(rata, 2);
+            lstOut.Items.Add("Rata - rata penjualan = " + hasilRata);
+
+
+
+        }
+
+        private void btnDiAtas1_Click(object sender, EventArgs e)
+        {
+            double rata = 0, total=0;
+            for (int i = 0; i < jumlahInput; i++)
+            {
+                total = total + arrJumlah[i];
+            }
+            rata = total / jumlahInput;
+            int under = 0,upper = 0;
+
+            for (int i = 0; i < jumlahInput; i++)
+            {
+                if (arrJumlah[i] < rata)
+                {
+                    under++;
+                }
+                else
+                {
+                    upper++;
+                }
+            }
+            double hasilRata = Math.Round(rata, 2);
+            lstOut.Items.Add("Rata-rata penjualan = " + hasilRata);
+            lstOut.Items.Add("Jumlah hari yang melebihi rata-rata : " + upper);
+            lstOut.Items.Add("Jumlah hari kurang dari rata-rata : " + under);
+
+        }
+
+        private void btnDiAtas2_Click(object sender, EventArgs e)
+        {
+            double rata = 0, total = 0;
+            for (int i = 0; i < jumlahInput; i++)
+            {
+                total = total + arrJumlah[i];
+            }
+            rata = total / jumlahInput;
+
+            double hasilRata = Math.Round(rata, 2);
+            lstOut.Items.Add("Rata-rata penjualan = " + hasilRata);
+
+            lstOut.Items.Add("Penjualan yang melebihi rata-rata : ");
+            for (int i = 0; i < jumlahInput; i++)
+            {
+                if (arrJumlah[i] > rata)
+                {
+                    lstOut.Items.Add("Tanggal " + arrTanggal[i] + " bulan " + arrBulan[i] + " : " + arrJumlah[i]);
+                }
+            }
+        }
+    }
+}
