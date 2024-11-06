917804601aeeeaf6d3a4aac012195d600b92ffc0
valenncollege <s160424066@student.ubaya.ac.id>
2024-11-06 18:33:27 +0700

    Add files via upload

---
+++ w9b/no1-4.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Tugas_W9B_Jevon_Valentino_160424066
+{
+    partial class tugas_w9b
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTanggal = new System.Windows.Forms.Label();
+            this.lblBulan = new System.Windows.Forms.Label();
+            this.lblJumlah = new System.Windows.Forms.Label();
+            this.txtTanggal = new System.Windows.Forms.TextBox();
+            this.txtBulan = new System.Windows.Forms.TextBox();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnTampil = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDiAtas2 = new System.Windows.Forms.Button();
+            this.btnDiAtas1 = new System.Windows.Forms.Button();
+            this.btnRata = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTanggal
+            // 
+            this.lblTanggal.AutoSize = true;
+            this.lblTanggal.Location = new System.Drawing.Point(76, 37);
+            this.lblTanggal.Name = "lblTanggal";
+            this.lblTanggal.Size = new System.Drawing.Size(97, 13);
+            this.lblTanggal.TabIndex = 0;
+            this.lblTanggal.Text = "Inputkan Tanggal :";
+            // 
+            // lblBulan
+            // 
+            this.lblBulan.AutoSize = true;
+            this.lblBulan.Location = new System.Drawing.Point(76, 89);
+            this.lblBulan.Name = "lblBulan";
+            this.lblBulan.Size = new System.Drawing.Size(85, 13);
+            this.lblBulan.TabIndex = 1;
+            this.lblBulan.Text = "Inputkan Bulan :";
+            // 
+            // lblJumlah
+            // 
+            this.lblJumlah.AutoSize = true;
+            this.lblJumlah.Location = new System.Drawing.Point(76, 138);
+            this.lblJumlah.Name = "lblJumlah";
+            this.lblJumlah.Size = new System.Drawing.Size(141, 13);
+            this.lblJumlah.TabIndex = 2;
+            this.lblJumlah.Text = "Inputkan Jumlah Penjualan :";
+            // 
+            // txtTanggal
+            // 
+            this.txtTanggal.Location = new System.Drawing.Point(312, 37);
+            this.txtTanggal.Name = "txtTanggal";
+            this.txtTanggal.Size = new System.Drawing.Size(100, 20);
+            this.txtTanggal.TabIndex = 3;
+            // 
+            // txtBulan
+            // 
+            this.txtBulan.Location = new System.Drawing.Point(312, 89);
+            this.txtBulan.Name = "txtBulan";
+            this.txtBulan.Size = new System.Drawing.Size(100, 20);
+            this.txtBulan.TabIndex = 4;
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(312, 138);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(100, 20);
+            this.txtJumlah.TabIndex = 5;
+            // 
+            // lstOut
+            // 
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.Location = new System.Drawing.Point(79, 210);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(333, 95);
+            this.lstOut.TabIndex = 6;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(492, 47);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 7;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnTampil
+            // 
+            this.btnTampil.Location = new System.Drawing.Point(492, 89);
+            this.btnTampil.Name = "btnTampil";
+            this.btnTampil.Size = new System.Drawing.Size(75, 23);
+            this.btnTampil.TabIndex = 8;
+            this.btnTampil.Text = "Tampil";
+            this.btnTampil.UseVisualStyleBackColor = true;
+            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(492, 138);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 9;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(601, 189);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDiAtas2
+            // 
+            this.btnDiAtas2.Location = new System.Drawing.Point(601, 89);
+            this.btnDiAtas2.Name = "btnDiAtas2";
+            this.btnDiAtas2.Size = new System.Drawing.Size(75, 23);
+            this.btnDiAtas2.TabIndex = 11;
+            this.btnDiAtas2.Text = "Di atas 2";
+            this.btnDiAtas2.UseVisualStyleBackColor = true;
+            // 
+            // btnDiAtas1
+            // 
+            this.btnDiAtas1.Location = new System.Drawing.Point(601, 47);
+            this.btnDiAtas1.Name = "btnDiAtas1";
+            this.btnDiAtas1.Size = new System.Drawing.Size(75, 23);
+            this.btnDiAtas1.TabIndex = 12;
+            this.btnDiAtas1.Text = "Di atas 1";
+            this.btnDiAtas1.UseVisualStyleBackColor = true;
+            // 
+            // btnRata
+            // 
+            this.btnRata.Location = new System.Drawing.Point(601, 136);
+            this.btnRata.Name = "btnRata";
+            this.btnRata.Size = new System.Drawing.Size(75, 23);
+            this.btnRata.TabIndex = 13;
+            this.btnRata.Text = "Rata2";
+            this.btnRata.UseVisualStyleBackColor = true;
+            this.btnRata.Click += new System.EventHandler(this.btnRata_Click);
+            // 
+            // tugas_w9b
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRata);
+            this.Controls.Add(this.btnDiAtas1);
+            this.Controls.Add(this.btnDiAtas2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnTampil);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.txtBulan);
+            this.Controls.Add(this.txtTanggal);
+            this.Controls.Add(this.lblJumlah);
+            this.Controls.Add(this.lblBulan);
+            this.Controls.Add(this.lblTanggal);
+            this.Name = "tugas_w9b";
+            this.Text = "Laporan Penjualan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTanggal;
+        private System.Windows.Forms.Label lblBulan;
+        private System.Windows.Forms.Label lblJumlah;
+        private System.Windows.Forms.TextBox txtTanggal;
+        private System.Windows.Forms.TextBox txtBulan;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.ListBox lstOut;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDiAtas2;
+        private System.Windows.Forms.Button btnDiAtas1;
+        private System.Windows.Forms.Button btnRata;
+    }
+}
